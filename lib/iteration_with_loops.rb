aafb07e8134c4020d11377dc87c3f13bba858b03
Carrie Bares <caroltbares@gmail.com>
2020-12-20 22:11:47 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,15 +1,15 @@
 def join_nested_strings(src)
   i = 0
-  return_string = ""
+  return_string = []
   while i < src.length
     i2 = 0
     while i2 < src[i].length
       if src[i][i2].class == String
-        return_string += src[i][i2]
+        return_string << src[i][i2]
       end
       i2 += 1
     end
     i += 1
   end
-  return return_string
+  return return_string.join(" ")
 end
